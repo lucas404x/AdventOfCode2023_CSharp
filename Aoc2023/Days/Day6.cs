5f6c5248aa93487229c587ff1e6909374f501a73
José Lucas da Silva <joselucas0303@gmail.com>
2023-12-14 20:37:46 -0300

    feat(day6): second half

--- Aoc2023/Days/Day6.cs
+++ Aoc2023/Days/Day6.cs
@@ -1,4 +1,6 @@
-﻿namespace Aoc2023.Days;
+﻿using System;
+
+namespace Aoc2023.Days;
 
 internal class Day6 : Day
 {
@@ -20,13 +22,30 @@ internal class Day6 : Day
             double delta = Math.Sqrt((raceTime * raceTime) - 4 * c);
 
             // Calculate the number of ways to beat the race.
-            int n1 = (int)Math.Round((-raceTime + delta) / 2);
-            int n2 = (int)Math.Round((-raceTime - delta) / 2);
+            int x1 = (int)Math.Round((-raceTime + delta) / 2);
+            int x2 = (int)Math.Round((-raceTime - delta) / 2);
 
             // Update the total number of ways to beat the race.
-            totalWaysToBeatRace *= (n1 - n2) + 1;
+            totalWaysToBeatRace *= (x1 - x2) + 1;
         }
 
         return totalWaysToBeatRace.ToString();
+    }
+
+    public override string SecondHalf()
+    {
+        var raceTime = long.Parse(Input[0].Split(':', StringSplitOptions.RemoveEmptyEntries)[1]);
+        var recordDist = long.Parse(Input[1].Split(':', StringSplitOptions.RemoveEmptyEntries)[1]);
+
+        // Calculate 'c' and 'delta' for the quadratic formula.
+        // The 'c' cannot be the 'recordDist' only, we need to get the next value from the fn to retrieve the right Xs.
+        double c = (recordDist + (recordDist / raceTime));
+        double delta = Math.Sqrt((raceTime * raceTime) - 4 * c);
+
+        // Calculate the number of ways to beat the race.
+        int x1 = (int)Math.Round((-raceTime + delta) / 2);
+        int x2 = (int)Math.Round((-raceTime - delta) / 2);
+
+        return ((x1 - x2) - 1).ToString();
     }
 }
