96bcb295353b597a07f9ccfe3b36970db900fe45
José Lucas da Silva <joselucas0303@gmail.com>
2023-12-14 02:28:32 -0300

    feat(day6): first half

---
+++ Aoc2023/Days/Day6.cs
@@ -0,0 +1,28 @@
+﻿namespace Aoc2023.Days;
+
+internal class Day6 : Day
+{
+    public override string FirstHalf()
+    {
+        int numberOfWaysToBeatRace = 1;
+
+        string[] times = Input[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] distances = Input[1].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < times.Length; i++)
+        {
+            var raceTime = long.Parse(times[i]);
+            var recordDist = long.Parse(distances[i]);
+
+            double c = (recordDist + (recordDist / raceTime));
+            double delta = Math.Sqrt((-raceTime * -raceTime) - 4 * c);
+
+            int n1 = (int)Math.Round((-raceTime + delta) / 2);
+            int n2 = (int)Math.Round((-raceTime - delta) / 2);
+
+            numberOfWaysToBeatRace *= (n1 - n2) + 1;
+        }
+
+        return numberOfWaysToBeatRace.ToString();
+    }
+}
